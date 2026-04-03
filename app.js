ba3da957851f87169d3ef6ab56b0b55f14c81774
Maja Lie <maja.lie@code.berlin>
2026-04-03 20:40:41 +0200

    Install ejs, set as templating engine. Add urlencoded middleware

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`);
 });
+
+app.use(express.static('public'));
+
+// Built-in middleware, allows us to read data sent through HTML forms
+app.use(express.urlencoded({ extended: true }));
+
+// Tells Express to use EJS as a templating engine
+app.set('view engine', 'ejs');
