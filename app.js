13ae36fcce533bed809a63e038a12388c6268590
Maja Lie <maja.lie@code.berlin>
2026-04-03 22:16:10 +0200

    Import router in all controller files, import controller files to app.js and add route functions

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from 'express';
+
+// Import config files
 import { PORT } from './config/app.js';
 import './config/database.js';
+
+// Import controller files
+import simpleRoutes from './controllers/simple-pages.js';
+import certificatesRoutes from './controllers/certificates.js';
+import loginRoutes from './controllers/login.js';
+import userRoutes from './controllers/user.js';
 
 const app = express();
 
@@ -11,6 +19,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // Tells Express to use EJS as a templating engine
 app.set('view engine', 'ejs');
+
+// Add route functions
+app.use(simpleRoutes);
+app.use(certificatesRoutes);
+app.use(loginRoutes);
+app.use(userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Started server on port ${PORT}`);
