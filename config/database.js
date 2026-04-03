177ba5bf9d035acfdab28fecd371355ede7926b0
Maja Lie <maja.lie@code.berlin>
2026-04-04 00:39:24 +0200

    Add error message

--- config/database.js
+++ config/database.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 /* import { MONGODB_URI } from "./app"; */
 
 /* mongoose.connect(MONGODB_URI)
-    .then(() => console.log('Database connected'))
-    .catch(error => console.error(error)) */
+    .then(result => {
+        console.log('Database connected')
+    })
+    .catch(error => {
+        console.log('error connecting to MongoDB: ', error.message)
+    })*/
+
+mongoose.set('strictQuery', false);
 
 mongoose.connect('mongodb://127.0.0.1:27017/bondeappen')
     .then(() => console.log('Database connected'))
