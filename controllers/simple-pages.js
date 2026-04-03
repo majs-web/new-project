1696da955bb6671ab5d9db87a8dc5e2dab565614
Maja Lie <maja.lie@code.berlin>
2026-04-04 00:40:11 +0200

    Add test route

--- controllers/simple-pages.js
+++ controllers/simple-pages.js
@@ -3,4 +3,9 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
+});
+
+
 export default router;
