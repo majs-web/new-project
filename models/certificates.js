2770c518e29f24cfb72d63e13b31c8c2ea051cd2
Maja Lie <maja.lie@code.berlin>
2026-04-04 19:18:35 +0200

    Add schema for users. Add user rerference to certificate file, for identifying connected user and certificate

--- models/certificates.js
+++ models/certificates.js
@@ -9,16 +9,20 @@ const certificateSchema = new mongoose.Schema({
         required: true
     },
     important: Boolean,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 // minLength and required validators are built into Mongoose
 
 // certificateSchema is an object --> should be turned into a string
-/* certificateSchema.set('toJSON', {
+certificateSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
     }
-}); */
+});
 
 export const Certificate = mongoose.model('Certificate', certificateSchema);
