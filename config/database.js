8887fe23bd6f6f45c260bac38e8948d3612746c4
Maja Lie <maja.lie@code.berlin>
2026-04-05 15:42:33 +0200

    Remove function

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ import mongoose from "mongoose";
         console.log('error connecting to MongoDB: ', error.message)
     })*/
         
-mongoose.set('strictQuery', false);
+/* mongoose.set('strictQuery', false); */
 
 mongoose.connect('mongodb://127.0.0.1:27017/bondeappen')
     .then(() => console.log('Database connected'))
