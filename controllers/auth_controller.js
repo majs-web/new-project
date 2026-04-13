1b355bf93572b275f56a4ab81051cf9ac56bdb12
Maja Lie <maja.lie@code.berlin>
2026-04-13 18:00:00 +0200

    Add new route files for better overview. Create functions for user creation and login, route handling

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,4 +1,5 @@
 
+import { User } from '../models/user1.js';
 import jwt from 'jsonwebtoken';
 
 // handle errors
@@ -39,4 +40,52 @@ const createToken = (id) => {
     return jwt.sign({ id }, 'secret-should-be-long-dont-post-it', {
         expiresIn: maxAge
     });
+}
+
+export const signup_get = (request, response) => {
+    response.render('signup');
+}
+
+export const login_get = (request, response) => {
+    response.render('login');
+}
+
+export const signup_post = async (request, response) => {
+    const { email, username, password } = request.body;
+
+    try {
+        const user = await User.create({ email, username, password });
+        const token = createToken(user._id);
+        response.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        response.status(201).json({ user: user._id });
+    }
+    catch (error) {
+        const errors = handleErrors(error);
+        response.status(400).json({ errors });
+    };
+};
+
+// When login correct -> cookie (jwt)
+export const login_post = async (request, response) => {
+    const { email, password } = request.body;
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        response.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        response.status(200).json({ user: user._id });
+    }
+    catch (error) {
+        const errors = handleErrors(error);
+        response.status(400).json({ errors });
+    }
+}
+
+// Replaces the jwt cookie which lets the user stay logged in
+// Empty string value removes/replaces the token value
+// Expires very quickly (1ms)
+export const logout_get = (request, response) => {
+    response.cookie('jwt', '', { maxAge: 1 });
+
+    response.redirect('/');
 }
