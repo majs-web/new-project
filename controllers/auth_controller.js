5b5f1a3d1460f86de9755e6f0ae1a36a017c80ae
Maja Lie <maja.lie@code.berlin>
2026-04-14 16:13:13 +0200

    Add jwt key to .env, import. Put routes and route functions back together. Remove unused error handling code

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,91 +1,76 @@
 
-import { User } from '../models/user1.js';
+import { Router } from 'express';
+import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "../config/app.js";
 
-// handle errors
-
-const handleErrors = (error) => {
-    console.log(error.message, error.code);
-    let errors = { email: '', username: '', password: ''};
-
-    // incorrect email
-    if (error.message === 'incorrect email') {
-        errors.email = 'that email is not registered';
-    }
-
-    // incorrect password
-    if (error.message === 'incorrect password') {
-        errors.password = 'that password is incorrect';
-    }
-
-    // duplicate error code
-    if (error.code === 11000) {
-        errors.email = 'that email is already registered';
-        return errors;
-    }
-
-    // validation errors
-    if (error.message.includes('user validation failed')) {
-        Object.values(error.errors).forEach(({properties}) => {
-            error[properties.path] = properties.message;
-        });
-    }
-
-    return errors;
-};
-
+const router = Router();
 const maxAge = 3 * 24 * 60 * 60;
 
 const createToken = (id) => {
-    return jwt.sign({ id }, 'secret-should-be-long-dont-post-it', {
-        expiresIn: maxAge
-    });
+    return jwt.sign({ id }, JWT_SECRET, { expiresIn: maxAge });
 }
 
-export const signup_get = (request, response) => {
+router.get('/signup', (request, response) => {
     response.render('signup');
-}
+})
 
-export const login_get = (request, response) => {
+router.get('/login', (request, response) => {
     response.render('login');
-}
+})
 
-export const signup_post = async (request, response) => {
-    const { email, username, password } = request.body;
-
+router.post('/signup', async (request, response) => {
     try {
+        const { email, username, password } = request.body;
         const user = await User.create({ email, username, password });
         const token = createToken(user._id);
-        response.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        response.status(201).json({ user: user._id });
-    }
-    catch (error) {
-        const errors = handleErrors(error);
-        response.status(400).json({ errors });
+
+        response
+            .cookie('jwt', token, {
+                httpOnly: true, 
+                sameSite: 'lax',
+                // set to true when deployed
+                secure: false,
+                maxAge: maxAge * 1000 
+            });
+            response.status(201).json({ user: user._id });
+    } catch (error) {
+        console.log('Signup error: ', error.message);
+        response.status(400).json({ error: error.message });
     };
-};
+});
 
 // When login correct -> cookie (jwt)
-export const login_post = async (request, response) => {
-    const { email, password } = request.body;
-
+router.post('/login', async (request, response) => {
     try {
+        const { email, password } = request.body;
         const user = await User.login(email, password);
         const token = createToken(user._id);
-        response.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        response.status(200).json({ user: user._id });
+
+        response.cookie('jwt', token, { 
+            httpOnly: true, 
+            sameSite: 'lax',
+            // set to true when deployed
+            secure: false,
+            maxAge: maxAge * 1000 
+        })
+        response.status(200).json({ 
+            user: user._id, 
+            email: user.email, 
+            username: user.username 
+        });
+    } catch (error) {
+        response.status(400).json({ error: error.message });
     }
-    catch (error) {
-        const errors = handleErrors(error);
-        response.status(400).json({ errors });
-    }
-}
+})
 
 // Replaces the jwt cookie which lets the user stay logged in
 // Empty string value removes/replaces the token value
 // Expires very quickly (1ms)
-export const logout_get = (request, response) => {
+router.get('/logout', (request, response) => {
     response.cookie('jwt', '', { maxAge: 1 });
 
-    response.redirect('/');
-}
+    return response.redirect('/');
+})
+
+export default router;
