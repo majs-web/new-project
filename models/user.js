6c4b8716b02217c4d13b590f3f5d5c9b8ca86912
Maja Lie <maja.lie@code.berlin>
2026-04-14 16:28:11 +0200

    Change isEmail import to import correctly. Add certificate to model so users only access their own certificates. Remove post function, unnecessary. Add to-JSON function from old user.js document

--- models/user.js
+++ models/user.js
@@ -1,36 +1,61 @@
 
 import mongoose from 'mongoose';
+import isEmail from 'validator/lib/isEmail.js';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
+    email: {
+        type: String, 
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+    },
     username: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'Please enter a username'], 
+        unique: true,
     },
-    name: String,
-/*     passwordHash: String, */
     password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters']
     },
     certificates: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Certificate'
         }
-    ],
+    ]
 });
+
+// function for before document saved to db
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// static message to login user
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password)
+        if (auth) {
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+} 
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        if (returnedObject._id) {
-            returnedObject.id = returnedObject._id.toString()
-        }
+        returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
-        delete returnedObject.__v
-        delete returnedObject.passwordHash
+        delete returnedObject._v
+        delete returnedObject.password
     }
-});
+})
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model('user', userSchema);
