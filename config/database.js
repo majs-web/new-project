1baed46fb9304d1da1ab1245dc4d5bf0248235ef
Maja Lie <maja.lie@code.berlin>
2026-04-14 16:13:36 +0200

    Remove unused code

--- config/database.js
+++ config/database.js
@@ -9,11 +9,5 @@ mongoose.connect(MONGODB_URI)
     .catch(error => {
         console.log('error connecting to MongoDB: ', error.message)
     })
-        
-/* mongoose.set('strictQuery', false); */
-
-/* mongoose.connect('mongodb://127.0.0.1:27017/bondeappen')
-    .then(() => console.log('Database connected'))
-    .catch(error => console.error(error)) */
 
 export const db = mongoose.connection;
