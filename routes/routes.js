b077317efd82d1bc0792dbfbd705dc3740c59a2e
Maja Lie <maja.lie@code.berlin>
2026-04-14 16:47:42 +0200

    Delete unused files and code

--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-
-import { requireAuth, checkUser } from "../middlewares/auth_middleware";
-
-app.get('*', checkUser)
-app.get('/', (request, response) => response.render('home'));
-app.get('/profile', requireAuth, (request, response) => response.render('profile'));
