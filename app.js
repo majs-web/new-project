cfc7f2234e9c2eafece1e26d896e9c1090cafab9
Maja Lie <maja.lie@code.berlin>
2026-04-14 16:46:58 +0200

    Import checkUser, add to all GET routes. Move route import to after. Remove unused code

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 import express from 'express';
 
 // Import middleware
-import { errorHandler, requestLogger, unkownEndpoint } from './middlewares/logger.js';
 import cookieParser from 'cookie-parser';
+import { checkUser } from './middlewares/auth_middleware.js';
 
 // Import config files
 import './config/database.js';
-
-// Import controller files
-import simpleRouter from './controllers/simple-pages.js';
-import certificateRouter from './controllers/certificates.js';
-import loginRouter from './controllers/login.js';
-import usersRouter from './controllers/users.js';
-
-import authRouter from './routes/auth_routes.js';
 
 const app = express();
 
@@ -29,27 +21,26 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+app.use((request, response, next) => {
+    if (request.method === 'GET') {
+        return checkUser(request, response, next);
+    }
+    next();
+});
+
+import simpleRouter from './controllers/routes.js';
+import authRouter from './controllers/auth_controller.js';
+import certRouter from './controllers/certificate_controller.js';
+
 // Tells Express to use EJS as a templating engine
 app.set('view engine', 'ejs');
 
 // Tells Express to look for ejs files in 'views' folder
 app.set('views', 'views');
 
-// Has to be used after json parser for request body to be initialized when logger is executed
-app.use(requestLogger);
-
 // Add route functions
 app.use(simpleRouter);
-app.use('/profile/certificates', certificateRouter); // 
-app.use(loginRouter);
-app.use('/signup', usersRouter);
-/* app.use('/profile', profileRouter); */
-
-app.use(authRouter)
-
-// This middleware after routes to catch requests made to non-existent routes
-app.use(unkownEndpoint);
-
-app.use(errorHandler);
+app.use(authRouter);
+app.use(certRouter);
 
 export const App = app;
